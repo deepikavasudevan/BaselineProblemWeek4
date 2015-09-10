b6eb44e0bb2ed54c2cce18710729c783517aa8ec
deepika <deepikav2793@gmail.com>
2015-09-10 12:36:13 +0530

    Renamed Cashier.java to ShoppingBasket.java to express intent better

---
+++ src/com/thoughtworks/baseline/ShoppingBasket.java
@@ -0,0 +1,38 @@
+/*A Cashier finds the total cost and sales tax of a shopping basket*/
+package com.thoughtworks.baseline;
+
+import java.util.ArrayList;
+
+public class ShoppingBasket {
+
+    private ArrayList<String> itemDetails = new ArrayList<>();
+
+    public ShoppingBasket(ArrayList<String> itemDetails) {
+        this.itemDetails = itemDetails;
+    }
+
+    public double salesTax() {
+        double salesTax = 0.0;
+
+        for (String itemDetail : itemDetails) {
+            Item item = convertItemDetailsIntoAnItem(itemDetail);
+            salesTax += item.salesTax() + item.importDuty();
+        }
+        return salesTax;
+    }
+
+    private Item convertItemDetailsIntoAnItem(String itemDetail) {
+        Parser parser = new Parser(itemDetail);
+        return parser.parse();
+    }
+
+    public double cost() {
+        double cost = 0.0;
+
+        for (String itemDetail : itemDetails) {
+            Item item = convertItemDetailsIntoAnItem(itemDetail);
+            cost += item.totalCost();
+        }
+        return cost;
+    }
+}
---
+++ test/com/thoughtworks/baseline/ShoppingBasketTest.java
@@ -0,0 +1,27 @@
+package com.thoughtworks.baseline;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class ShoppingBasketTest {
+
+    @Test
+    public void shouldCalculateSalesTaxOfShoppingBasket() {
+        ArrayList<String> itemDetails = new ArrayList<>(Arrays.asList("1 book at 12.49", "1 music CD at 14.99", "1 chocolate bar at 0.85"));
+        ShoppingBasket shoppingBasket = new ShoppingBasket(itemDetails);
+
+        assertEquals(1.50, shoppingBasket.salesTax(), 0.001);
+    }
+
+    @Test
+    public void shouldCalculateCostOfShoppingBasket() {
+        ArrayList<String> itemDetails = new ArrayList<>(Arrays.asList("1 book at 12.49", "1 music CD at 14.99", "1 chocolate bar at 0.85"));
+        ShoppingBasket shoppingBasket = new ShoppingBasket(itemDetails);
+
+        assertEquals(29.83, shoppingBasket.cost(), 0.001);
+    }
+}
